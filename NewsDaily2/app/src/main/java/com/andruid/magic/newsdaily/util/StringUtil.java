3186a991a5c5618aaab6ff4f76b61be64ae1218b
DeaDshot14 <mistry14darren@gmail.com>
2019-07-27 16:12:54 +0530

    clear

--- NewsDaily2/app/src/main/java/com/andruid/magic/newsdaily/util/StringUtil.java
+++
@@ -1,7 +0,0 @@
-package com.andruid.magic.newsdaily.util;
-
-public class StringUtil {
-    public static String capFirstLetter(String str) {
-        return Character.toUpperCase(str.charAt(0)) + str.substring(1);
-    }
-}
--- NewsDaily2/texttospeech/src/main/java/com/andruid/magic/texttospeech/data/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.andruid.magic.texttospeech.data;
-
-public class Constants {
-    public static final String DIR_TTS = "ttsNews";
-}
