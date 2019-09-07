6beda34afe1bebc10f59ef3bde81535927e1ca25
DeaDshot14 <mistry14darren@gmail.com>
2019-09-07 16:48:56 +0530

    v3

--- NewsDaily2/app/src/main/java/com/andruid/magic/newsdaily/activity/MainActivity.java
+++ NewsDaily2/app/src/main/java/com/andruid/magic/newsdaily/activity/MainActivity.java
@@ -1,16 +1,25 @@
 package com.andruid.magic.newsdaily.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
 import com.andruid.magic.newsdaily.R;
+import com.andruid.magic.newsdaily.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding.unbind();
     }
 }
---
+++ NewsDaily3/app/src/main/java/com/andruid/magic/newsdaily/MainActivity.java
@@ -0,0 +1,14 @@
+package com.andruid.magic.newsdaily;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
